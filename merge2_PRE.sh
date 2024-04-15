0f527691cfd504693c352036d830d0b3b736525f
JamieCFreeman <jcf236@cornell.edu>
2024-04-15 16:35:26 -0500

    cleaning up

--- merge2_PRE.sh
+++ merge2_PRE.sh
@@ -3,9 +3,6 @@
 # Argument is sample code
 INPUT=$1
 
-# Append folder name onto file list so they can be found by tar
-#sed -i 's ^ outputs/ ' ${INPUT}
-
 # Tar input files and send archive to staging
 tar -cvf ${INPUT}.tar.gz /staging/jcfreeman2/merge_list${1}*.bam
 mv ${INPUT}.tar.gz /staging/jcfreeman2/
