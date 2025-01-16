52a54dbecac9153ab9d290371906660049d35a8e
JamieCFreeman <jcf236@cornell.edu>
2025-01-16 09:38:40 -0600

    add --remove-files flag when creating tar archive for merge to reduce disk usage

--- gather_merge.sh
+++ gather_merge.sh
@@ -3,7 +3,8 @@
 # Argument islist of files to merge
 INPUT=$1
 
-# Tar input files and send archive to staging
-tar -cf ${INPUT}.tar.gz -T ${INPUT}
+# Tar input files and send archive to staging,
+#	upon successful archive creation, delete block files
+tar --remove-files -cf ${INPUT}.tar.gz -T ${INPUT}
 mv ${INPUT}.tar.gz /staging/jcfreeman2/
 
--- merge2_PRE.sh
+++ merge2_PRE.sh
@@ -4,6 +4,6 @@
 INPUT=$1
 
 # Tar input files and send archive to staging
-tar -cvf ${INPUT}.tar.gz /staging/jcfreeman2/merge_list${1}*.bam
+tar --remove-files -cvf ${INPUT}.tar.gz /staging/jcfreeman2/merge_list${1}*.bam
 mv ${INPUT}.tar.gz /staging/jcfreeman2/
 
