03e3edc37bc2671772be36db12ad6ee1a0093b78
JamieCFreeman <jcf236@cornell.edu>
2025-05-12 17:57:25 -0500

    add error checking, return exit value 1 when not all expected bams present for merging

--- gather_merge.sh
+++ gather_merge.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Argument islist of files to merge
+# Argument is list of files to merge
 INPUT=$1
+
+# Check whether all files exist
+ALL_HERE=`python scripts/all_exist.py ${INPUT}`
+
+if [[ "${ALL_HERE}" == "True" ]]; then
+  echo "go"
+  # Tar input files and send archive to staging,
+  #	upon successful archive creation, delete block files
+  tar --remove-files -cf ${INPUT}.tar.gz -T ${INPUT}
+  mv ${INPUT}.tar.gz /staging/jcfreeman2/
+else
+  echo "Some input files are missing for merge ${INPUT}"
+  exit 1 # terminate and indicate error
+fi
 
 # Tar input files and send archive to staging,
 #	upon successful archive creation, delete block files
-tar --remove-files -cf ${INPUT}.tar.gz -T ${INPUT}
-mv ${INPUT}.tar.gz /staging/jcfreeman2/
+#tar --remove-files -cf ${INPUT}.tar.gz -T ${INPUT}
+#mv ${INPUT}.tar.gz /staging/jcfreeman2/
 
